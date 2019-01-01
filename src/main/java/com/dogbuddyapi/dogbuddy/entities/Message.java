03f19a18a45a1eb1526e5350230aa827953cf12d
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-12-31 17:35:39 -0800

    Added setters and getters to message entity

--- src/main/java/com/dogbuddyapi/dogbuddy/entities/Message.java
+++ src/main/java/com/dogbuddyapi/dogbuddy/entities/Message.java
@@ -10,8 +10,50 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String to;
     private String from;
     private String subject;
     private String messageContent;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessageContent() {
+        return messageContent;
+    }
+
+    public void setMessageContent(String messageContent) {
+        this.messageContent = messageContent;
+    }
+
 }
