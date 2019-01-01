b1331b79dbe6a55d675cb8e618627974351f26f9
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-12-31 16:32:25 -0800

    Created app template

---
+++ src/main/java/com/dogbuddyapi/dogbuddy/DogbuddyApplication.java
@@ -0,0 +1,14 @@
+package com.dogbuddyapi.dogbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogbuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogbuddyApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/dogbuddyapi/dogbuddy/DogbuddyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dogbuddyapi.dogbuddy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DogbuddyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
