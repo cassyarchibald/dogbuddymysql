79fd5cbe288a6c135fba08164fb7666a867d7347
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2019-01-04 11:56:13 -0800

    Updated playdate repo

--- src/main/java/com/dogbuddyapi/dogbuddy/repos/PlayDateRepository.java
+++ src/main/java/com/dogbuddyapi/dogbuddy/repos/PlayDateRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface PlayDateRepository extends CrudRepository<PlayDate, Long> {
+
+
 }
