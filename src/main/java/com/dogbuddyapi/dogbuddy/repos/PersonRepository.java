519f0120694090952c1c554232653e8e75aa73fe
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2019-01-04 11:34:01 -0800

    Added find by first name to person repo

--- src/main/java/com/dogbuddyapi/dogbuddy/repos/PersonRepository.java
+++ src/main/java/com/dogbuddyapi/dogbuddy/repos/PersonRepository.java
@@ -3,6 +3,10 @@ package com.dogbuddyapi.dogbuddy.repos;
 import com.dogbuddyapi.dogbuddy.entities.Person;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+    List<Person> findByFirstName(@Param("first_name") String name);
 }
