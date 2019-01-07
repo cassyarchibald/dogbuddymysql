3edb7b648950c9df822dcfa00356cb160588519a
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2019-01-06 20:08:44 -0800

    Added cors filter to allow localhost:3000 and 8080 to communicate

--- src/main/java/com/dogbuddyapi/dogbuddy/DogbuddyApplication.java
+++ src/main/java/com/dogbuddyapi/dogbuddy/DogbuddyApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.convert.Jsr310Converters;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
 @EntityScan(basePackageClasses = {DogbuddyApplication.class, Jsr310Converters.class})
+@CrossOrigin(origins = "http://localhost:3000")
 public class DogbuddyApplication {
 
     public static void main(String[] args) {
